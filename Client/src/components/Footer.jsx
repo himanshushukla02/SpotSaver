c7af697491a37c2fe7a4a5e191b5f8c9e5d5536e
Himanshu shukla <riderhimanshu2004@gmail.com>
2026-09-10 17:35:08 +0530

    Updated project

--- Client/src/components/Footer.jsx
+++ Client/src/components/Footer.jsx
@@ -3,8 +3,8 @@ import github from '/github.svg';
 export default function Footer() {
   return (
     <div className="text-xl text-white p-4 bg-primary text-center flex justify-center gap-2">
-        gitesh-singla
-        <a href="https://github.com/gitesh-singla" target="_blank"><img src={github} className="max-h-6 align-bottom"/></a>
+        Himanshu-shukla
+        <a href="https://github.com/himanshushukla02/SpotSaver" target="blank"><img src={github} className="max-h-6 align-bottom"/></a>
     </div>
   )
 }
